ca15504676ef67bec43d8a3c964773a1ac3f1dc4
Helen Hou-Sandi <helen.y.hou@gmail.com>
2018-06-15 22:16:14 -0400

    Flesh out README.md and fix plugin header

--- mce_table_buttons.php
+++ mce_table_buttons.php
@@ -1,13 +1,13 @@
 <?php
 /**
- Plugin Name: MCE Table Buttons
- Plugin URI: http://10up.com/plugins-modules/wordpress-mce-table-buttons/
- Description: Add <strong>controls for table editing</strong> to the visual content editor with this <strong>light weight</strong> plug-in.
- Version: 3.3
- Author: Jake Goldman, 10up, Oomph
- Author URI: http://10up.com
- License: GPLv2 or later
-*/
+ * Plugin Name: MCE Table Buttons
+ * Plugin URI: http://10up.com/plugins-modules/wordpress-mce-table-buttons/
+ * Description: Add <strong>controls for table editing</strong> to the visual content editor with this <strong>light weight</strong> plug-in.
+ * Version: 3.3
+ * Author: Jake Goldman, 10up, Oomph
+ * Author URI: http://10up.com
+ * License: GPLv2 or later
+ */
 
 class MCE_Table_Buttons {
 
